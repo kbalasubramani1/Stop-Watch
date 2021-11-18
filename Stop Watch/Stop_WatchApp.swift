959209473f1ec561d3d893de6acfc767d98f5fb3
Kavin Bala <kbalasubramani@bsd220.org>
2021-11-18 10:34:43 -0600

    Initial Commit

---
+++ Stop Watch/Stop_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stop_WatchApp.swift
+//  Stop Watch
+//
+//  Created by Student on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct Stop_WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
